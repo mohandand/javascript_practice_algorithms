b2f4d8425ad666b84765487675930b911b84f51a
mohandand <mohandand@gmail.com>
2021-10-27 23:32:58 -0700

    pushed code

---
+++ missingnumber.js
@@ -0,0 +1,13 @@
+var missingNumber = (nums) => {
+        let n = nums.length;
+        let arra = [];
+        for(i=0;i<=n;i++)
+        {
+            arra.push(i);
+        }
+        let nums = nums.join("");
+        for(i=0;i<arra.length;i++){
+            
+        }
+
+}
