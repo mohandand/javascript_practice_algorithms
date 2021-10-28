82050ea655cf0a1e06c6c641cff2fcb39313f05c
mohandand <mohandand@gmail.com>
2021-10-28 16:21:22 -0700

    pushed code

---
+++ javascriptthisconcept.js
@@ -0,0 +1,5 @@
+var myfunction = function () {
+    console.log(this);
+}
+
+myfunction();
--- missingnumber.js
+++ missingnumber.js
@@ -5,9 +5,13 @@ var missingNumber = (nums) => {
         {
             arra.push(i);
         }
-        let nums = nums.join("");
+        let nums1 = nums.join("");
         for(i=0;i<arra.length;i++){
-            
+            let status = nums1.includes(arra[i]);
+            if(!status){
+                return arra[i];
+            }
         }
-
 }
+
+console.log(missingNumber([44,26,34,25,23,42,0,43,38,14,47,19,49,6,16,41,24,35,10,4,32,5,8,15,31,3,46,22,2,30,28,37,1,21,39,45,9,48,36,17,7,27,18,29,13,40,11,20,12]));
