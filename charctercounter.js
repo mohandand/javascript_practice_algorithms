97667cbfec7893f8f25ec44579b07987463c71b4
mohandand <mohandand@gmail.com>
2021-11-12 23:31:22 -0800

    Updated Code

--- charctercounter.js
+++ charctercounter.js
@@ -15,3 +15,12 @@ function validateWord(s)
 }
 
 console.log(validateWord("abc123!!"));
+
+// function validateWord(s) {
+//     var freq = {}
+//     s.toLowerCase().split('').forEach(function(s) {
+//       freq[s] ? freq[s]++ : freq[s] = 1
+//     })
+  
+//     return new Set(Object.values(freq)).size == 1
+//   }
