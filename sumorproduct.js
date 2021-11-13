0e5fe9f965a46fe8437ca3e8698a7d1f309950f0
mohandand <mohandand@gmail.com>
2021-11-12 19:34:26 -0800

    Updated Code

--- sumorproduct.js
+++ sumorproduct.js
@@ -22,3 +22,12 @@ function sumOrProduct(array, n) {
     else return "same"
   }
   console.log(sumOrProduct([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4));
+
+// Approach-2
+
+//   const sumOrProduct = (arr, n) => {
+//     const sorted = arr.sort((a, b) => a - b);
+//     const sum = sorted.slice(-n).reduce((acc, i) => acc + i);
+//     const product = sorted.slice(0, n).reduce((acc, i) => acc * i, 1);
+//     return sum > product ? 'sum' : sum < product ? 'product' : 'same';
+//   }
