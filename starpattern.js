a46625b8bd205086926426a23ab69346c2705777
mohandand <mohandand@gmail.com>
2021-11-14 13:20:14 -0800

    Updated Code

--- starpattern.js
+++ starpattern.js
@@ -1,14 +1,4 @@
-let temp=0;
-let k = 0;
-//let count =0;
-for(let i =0 ; i<=3;i++){
-    let count =0;
-    for(let j=0;j<=i;j++){
-         if(count==0) k = i+1;
-        console.log(k);
-        k++;
-        count++;    
-    }
-    temp = temp+k;
+for(let triangle=1;triangle.length<=6;triangle=triangle + "*")
+{
+    console.log(triangle);
 }
-console.log(temp);
