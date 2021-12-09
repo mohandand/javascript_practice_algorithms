2b3874a08dfc6c6c6979b4a7988a65bc9abeb18b
mohandand <mohandand@gmail.com>
2021-12-09 14:40:39 -0800

    updated code

--- Practice/alphabetposition.js
+++ Practice/alphabetposition.js
@@ -1,11 +1,21 @@
 let alpha = "abcdefghijklmnopqrstuvwxyz";
 let alphaarry = alpha.split("");
 console.log(alphaarry);
+let alphaNums = [];
 
 function alphaposition(text){
-        let str = text.toLowerCase().replace(/^a-zA-Z/g,'');
-        let strarray = str.split("");
-        console.log(strarray);
+    text = text.toLowerCase();
+       for(let i = 0 ; i<text.length;i++){
+           let idx = alphaarry.indexOf(text[i])
+           if(idx===-1){
+               continue;
+           }
+           else{
+               alphaNums.push(idx+1)
+           }
+       }
+
+       return alphaNums;
 }
 
-console.log(alphaposition("Iam very shy2 write&read"));
+console.log(alphaposition("The Sunset isn't Good as it is..?&"));
