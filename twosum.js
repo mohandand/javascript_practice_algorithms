57fd6b26db7b52767598bc54c39f7929556713a4
mohandand <mohandand@gmail.com>
2021-12-11 16:26:08 -0800

    updated code

--- twosum.js
+++ twosum.js
@@ -1,23 +1,33 @@
-var twoSum = function(nums, target) {
+// var twoSum = function(nums, target) {
     
-    for(i=0 ; i<=nums.length ;i++)
-        {
-            for(j=1;j<=nums.length;j++)
-            {
+//     for(i=0 ; i<=nums.length ;i++)
+//         {
+//             for(j=1;j<=nums.length;j++)
+//             {
                 
-                if(nums[i] + nums[j] ==target && i!=j)
-                    {
-                        return arry = [i,j];
-                    }
-            }
-        }
+//                 if(nums[i] + nums[j] ==target && i!=j)
+//                     {
+//                         return arry = [i,j];
+//                     }
+//             }
+//         }
     
-};
+// };
 
 
-console.log(twoSum([2,5,5,11] , 10));
+// console.log(twoSum([2,5,5,11] , 10));
 
 //need to write twosum with object
 
-//asdsa
 
+var twoSum = function(nums,target){
+    var map = {};
+    for(var i=0;i<nums.length;i++){
+        var m = target-nums[i];
+        if(map[m] !==undefined){
+            return [map[m],i];
+        }
+        map[nums[i]]=i;
+    }
+}
+console.log(twoSum([2,5,5,11] , 10));
