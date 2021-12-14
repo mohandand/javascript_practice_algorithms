9194d54a8a498868b978a3343a11361975327745
mohandand <mohandand@gmail.com>
2021-12-13 19:56:18 -0800

    updated code

--- mergeintervals.js
+++ mergeintervals.js
@@ -3,6 +3,16 @@ var merge = function(intervals){
     intervals.sort(function(a,b) {
          return a[0] - b[0];
     });
-
+    for(let i=0;i<intervals.length;i++){
+        var pre = res[res.length-1];
+        if(i==0 || intervals[i][0]>pre[1])
+        {
+            res.push(intervals[i])
+        }
+        else{
+            pre[1] = Math.max(pre[1],intervals[i][1]);
+        }
+    }
+    return res;
 }
-console.log(merge[[1,2],[2,5],[5,10],[3,4]]);
+console.log(merge[[1,2],[2,5],[5,10],[3,7]]);
