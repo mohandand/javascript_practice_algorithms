fc94cd09f3be4ff659160758c6450e4326b034cf
mohandand <mohandand@gmail.com>
2021-12-15 15:58:40 -0800

    updated code

--- validparnthesis.js
+++ validparnthesis.js
@@ -1,32 +1,8 @@
-
-const symbols = {
-    "(" : ")",
-    "{" : "}",
-    "[": "]",
+var isValid = function(s){
+    let bracket = {
+        '(':')',
+        '{':'}',
+        '[':']'
+    }
+    let
 }
-const symbols1 = {
-    ")" : ")",
-    "}" : "}",
-    "]": "]",
-}
-var isValid = function(s) {
-
-    var count = 0;
-  
-  for(i=0;i<s.length;i++)
-  {
-      if(symbols[s[i]]===symbols1[s[i+1]]  )
-      {
-            i++;
-      } else if (symbols[s[i]]===symbols1[s[s.length - (i+1)]]){
-          
-        count++;
-      }
-      else{
-          return false;
-      }
-  }
-        return true;
-};
-
-console.log(isValid("){"));
