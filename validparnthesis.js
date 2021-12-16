8ee66939dc0bab8e3b61a03bfbd9d75d7be6eacb
mohandand <mohandand@gmail.com>
2021-12-15 16:06:54 -0800

    updated code

--- validparnthesis.js
+++ validparnthesis.js
@@ -5,4 +5,14 @@ var isValid = function(s){
         '[':']'
     }
     let heap= [];
+    for(char of s){
+        if(bracket[char]){
+            heap.push(bracket[char]);
+        }else{
+            if(!heap.pop()==char) return false
+        }
+    }
+    return (!heap.length)
 }
+
+console.log(isValid('(){}({})'));
