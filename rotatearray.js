9acae17e708081f592109c0590a339b689e4b0e1
mohandand <mohandand@gmail.com>
2021-12-26 17:05:43 -0800

    updated code

--- rotatearray.js
+++ rotatearray.js
@@ -1,11 +1,21 @@
-function rotatearray(array , r){
-    let res=[];
+// function rotatearray(array , r){
+//     let res=[];
 
-    for(let i=0 ; i<r ;i++){ 
-        array.unshift(array[array.length-1]);
-        array.pop();
-    }
-    return array;
-}
+//     for(let i=0 ; i<r ;i++){ 
+//         array.unshift(array[array.length-1]);
+//         array.pop();
+//     }
+//     return array;
+// }
+
+var rotatearray = function(nums, k) {
+    
+    let rotation = k % nums.length; //This is when k is greater than nums.length
+    if (rotation === 0) return; //No rotation is needed
+    
+    let tmp = nums.slice(0,nums.length - rotation);
+    nums.splice(0,nums.length - rotation);
+    nums.push(...tmp);
+};
 
 console.log(rotatearray([1,2,3,4,5,6,7],3))
