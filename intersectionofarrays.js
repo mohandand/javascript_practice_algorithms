518bcb0081d697e9ff32daacb0e076e794c4d720
mohandand <mohandand@gmail.com>
2021-12-27 22:28:58 -0800

    updated code

--- intersectionofarrays.js
+++ intersectionofarrays.js
@@ -1,15 +1,20 @@
 function intersectionofarrays(array1,array2){
 
     let obj1 ={};
-    let obj2 = {};
+    let res = [];
 
     for(i of array1){
         (obj1[i]) ? obj1[i]++ : obj1[i] = 1
     }
-
-    for(j of array2){
-        (obj2[j]) ? obj2[j]++ : obj2[j] =1 
-    }
     
-    for(k )
+    for(let k of array2){
+        if(obj1[k]>0){
+           res.push(k);
+           obj1[k]--;
+        }
+    }
+
+    return res;
 }
+
+console.log(intersectionofarrays([1,2,3,4,3],[1,2,3,3]));
