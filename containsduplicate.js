41e07d5647b888fc287bf950bde60bcfa2023cd9
mohandand <mohandand@gmail.com>
2021-12-27 21:31:34 -0800

    updated code

---
+++ containsduplicate.js
@@ -0,0 +1,16 @@
+function containsduplicate(array) {
+
+    let obj={};
+
+    for(let i of array){
+        (obj[i]) ? obj[i]++ :obj[i]=1
+    }
+    let keys = Object.keys(obj);
+
+    let res = keys.every((item) => item<=1)
+
+    return res;
+
+}
+
+console.log(containsduplicate([1,2,3,4,5]));
