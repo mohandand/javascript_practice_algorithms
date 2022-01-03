a1ca3d0bbc9d77499dcd2d297c4b36750418d5be
mohandand <mohandand@gmail.com>
2022-01-02 22:34:09 -0800

    updated code

--- rotateimage.js
+++ rotateimage.js
@@ -1,11 +1,14 @@
 
 function rotatematrix(matrix){
-    let arra=[];
   for(let r=0 ;r<matrix.length;r++){
         for(let c=r;c<matrix.length;c++){
            [matrix[r][c],matrix[c][r]] = [matrix[c][r],matrix[r][c]]
         }
   }
-}
+
+  for(let row of matrix){
+      row.reverse();
+  }
+};
 
 rotatematrix("sd");
