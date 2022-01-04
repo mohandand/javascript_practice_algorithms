18ace94fd273e236d1e7a522dcfbbfe2821777ab
mohandand <mohandand@gmail.com>
2022-01-03 19:04:23 -0800

    updated code

--- reversestring.js
+++ reversestring.js
@@ -31,3 +31,19 @@
 // console.log(revstring("RAJU"));
 
 //Approcah-4
+
+function revstring(s){
+    let left = 0;
+    let right = s.length-1;
+    while(left<right){
+        let hold = s[left];
+        s[left] = s[right];
+        s[right] =hold;
+        left++;
+        right--;
+    }
+
+    return s;
+}
+
+console.log(revstring("RAJU"));
