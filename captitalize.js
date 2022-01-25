ddd34d1ac2a1db06db845d60d38b7a44dcc2b32d
mohandand <mohandand@gmail.com>
2022-01-24 20:28:31 -0800

    Detect Capital Program

---
+++ captitalize.js
@@ -0,0 +1,23 @@
+function capitalize(title){
+    let words = title.split(" ");
+    let res = '';
+    for(let i = 0 ;i<words.length;i++){
+        if(words[i].length>1){
+            let temp =  words[i][0].toUpperCase() + words[i].slice(1,words[i].length+1).toLowerCase();
+            res = res + temp + ' ';
+        }else{
+            let temp = words[i].toLowerCase();
+            res = res + temp + ' ';
+        }
+    }   
+   return res.trim();//to remove space at start and end
+}
+
+console.log(capitalize("capiTalIze tHe titLe"))
+
+const  capitalizeTitle =(title)=> {
+    const words = title.toLowerCase().split(' ');
+   
+  return words.map((word,index)=>word.length  > 2  ?  word[0].toUpperCase() + word.slice(1) : word).join(" ")
+    
+  };
--- detectCapital.js
+++ detectCapital.js
@@ -1,19 +1,24 @@
 function detectCapital(str){
     let text = str.split('');
-    if(text[0] === text[0].toUpperCase){
-        if(str === str.toUpperCase){
+    if(text[0] === text[0].toUpperCase()){
+        if(str === str.toUpperCase()){
             return true;
         }
-        else if(str.slice(1, text.length+1) === str.toLowerCase){
+        else if(str.slice(1, text.length+1) === str.slice(1, text.length+1).toLowerCase()){
             return true;
         }else{
             return false;
         }
-    }else if (str === str.toLowerCase){
+    }else if (str === str.toLowerCase()){
         return true;
     }
     else{
         return false;
     }
 }
-console.log(detectCapital("USA"));
+console.log(detectCapital("Leetcode"));
+
+var detectCapitalUse = function(word) {
+    // either all capitals, all small cases, or Capital follow by small cases
+    return /^[A-Z]+$|^[a-z]+$|^[A-Z][a-z]+$/.test(word);
+};
