e76b802a5107e565137fccfa0784e2a50dbfbfa1
mohandand <mohandand@gmail.com>
2022-01-25 21:44:33 -0800

    Detect Capital Program

--- bracketsbalanced.js
+++ bracketsbalanced.js
@@ -4,5 +4,5 @@ var minSwaps = function (s) {
         ch == "[" ? ++count : --count; // level out [ & ]
         if (count < 0) count = 1; // reset to 1, we only care about ] on the right side
     }
-    return count / 2; // take half
+    return count / 2; // take half of it
 };
