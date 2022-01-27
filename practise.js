9578dfbcce6d7c650dde3396107259b4b4e7e599
mohandand <mohandand@gmail.com>
2022-01-27 14:23:39 -0800

    Detect Capital Program

--- practise.js
+++ practise.js
@@ -1,10 +1,2 @@
-let repeatedStringMatch = function(a, b) {
-    let num=Math.ceil(b.length/a.length);
-    if(a.repeat(num).includes(b)){
-        return num;
-    }
-    if(a.repeat(num+1).includes(b)){
-        return num+1;
-    }
-        return -1;
-};
+var obj;
+console.log(obj)
--- twosum.js
+++ twosum.js
@@ -1,3 +1,17 @@
+function twoSum(nums, target){
+    const comp = new Map();
+    const len  = nums.length;
+    for(let i =0 ; i<len ; i++){
+        if(comp[nums[i]]>=0){
+            return [comp[nums[i]], i]
+        }
+        comp[target-nums[i]] = i;
+    }
+
+    return [];
+}
+console.log(twoSum([2,5,6,11] , 8));
+
 // var twoSum = function(nums, target) {
     
 //     for(i=0 ; i<=nums.length ;i++)
@@ -20,17 +34,17 @@
 //need to write twosum with object
 
 
-var twoSum = function(nums,target){
-    var map = {};
-    for(var i=0;i<nums.length;i++){
-        var m = target-nums[i];
-        console.log(m);
-        if(map[m] !==undefined){
-            console.log(`${map[m]} and ${i}`);
-            console.log(map);
-            return [map[m],i];
-        }
-        map[nums[i]]=i;
-    }
-}
-console.log(twoSum([2,5,5,11] , 10));
+// var twoSum = function(nums,target){
+//     var map = {};
+//     for(var i=0;i<nums.length;i++){
+//         var m = target-nums[i];
+//         console.log(m);
+//         if(map[m] !==undefined){
+//             console.log(`${map[m]} and ${i}`);
+//             console.log(map);
+//             return [map[m],i];
+//         }
+//         map[nums[i]]=i;
+//     }
+// }
+// console.log(twoSum([2,5,5,11] , 10));
