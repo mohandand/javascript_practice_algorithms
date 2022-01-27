a1338ea1d0fa15577cd2792428557c1ea3e517ca
mohandand <mohandand@gmail.com>
2022-01-27 14:24:03 -0800

    Two Sum Program

--- twosum.js
+++ twosum.js
@@ -12,21 +12,21 @@ function twoSum(nums, target){
 }
 console.log(twoSum([2,5,6,11] , 8));
 
-// var twoSum = function(nums, target) {
+var twoSum = function(nums, target) {
     
-//     for(i=0 ; i<=nums.length ;i++)
-//         {
-//             for(j=1;j<=nums.length;j++)
-//             {
+    for(i=0 ; i<=nums.length ;i++)
+        {
+            for(j=1;j<=nums.length;j++)
+            {
                 
-//                 if(nums[i] + nums[j] ==target && i!=j)
-//                     {
-//                         return arry = [i,j];
-//                     }
-//             }
-//         }
+                if(nums[i] + nums[j] ==target && i!=j)
+                    {
+                        return arry = [i,j];
+                    }
+            }
+        }
     
-// };
+};
 
 
 // console.log(twoSum([2,5,5,11] , 10));
