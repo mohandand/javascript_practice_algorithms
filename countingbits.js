d67bca6a44c303723aa933a99c05da1f54e1dab9
mohandand <mohandand@gmail.com>
2022-02-01 15:24:08 -0800

    Count Digits of 1s

---
+++ countingbits.js
@@ -0,0 +1,17 @@
+var countBits = function(n) {
+    let dp = [0];
+    dp.length = n + 1;
+    offset =  1;
+    
+for(let i = 1 ;i<n+1 ; i++)
+{
+    if(offset * 2 == i) offset = i;
+    let dif = i - offset;
+    console.log(dif)
+    dp[i] = 1+dp[dif]
+}
+  return dp 
+    
+};
+
+console.log(countBits(3))
