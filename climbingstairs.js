6e6d2deb33103f7ae24a62d75730dc2a2b6b30eb
mohandand <mohandand@gmail.com>
2022-02-04 20:53:42 -0800

    Climbing Tree Program

---
+++ climbingstairs.js
@@ -0,0 +1,12 @@
+function climbTree(n){
+    let one  =1;
+    let two = 1;
+  let temp;
+    for(let i =0 ; i<n-1;i++){
+        temp = one;
+        one=one+two;
+        two=temp;
+    }
+
+    return one
+}
