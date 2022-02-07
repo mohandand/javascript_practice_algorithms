1f4378502c2635cb9b53431c7e488bb1dc1bbde7
mohandand <mohandand@gmail.com>
2022-02-06 21:34:01 -0800

    Climbing Tree Program

--- countingbits.js
+++ countingbits.js
@@ -1,12 +1,12 @@
 //dynamic programing
 var countBits = function(n) {
     let dp = [0]; 
-    dp.length = n + 1;
+    dp.length = n + 1; 
     offset =  1;
     
 for(let i = 1 ;i<n+1 ; i++)
 {
-    if(offset * 2 == i) offset = i;
+    if(offset * 2 == i) offset = i; //pattern detected
     let dif = i - offset;
     console.log(dif)
     dp[i] = 1+dp[dif]
