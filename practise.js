5814f71df98b1d11f471f4e09452a3de3b034cc9
mohandand <mohandand@gmail.com>
2022-02-28 18:50:49 -0800

    find the differnece

--- practise.js
+++ practise.js
@@ -1,6 +1,6 @@
 function findd(s,t){
     let ssort = s.split('').sort();
     let tsort = t.split('').sort();
-    return tsort.find((char,i) => char !==ssort[i]);
+    return tsort.find((char,i) => char !==ssort[i]); //find will return when ever it finds as per requirement
 }
 console.log(findd("abc","acd"))
