27b0f8d95ebdcd5f82151df4b2fb15c440da29e2
mohandand <mohandand@gmail.com>
2022-02-28 18:39:24 -0800

    add bits of number

--- practise.js
+++ practise.js
@@ -1,13 +1,6 @@
-function sumofDigits(num){
-    if(num<10) return num;
-    const numString = num.toString();
-    const numArray = numString.split('');
-    let sum=0;
-    for(let bit of numArray){
-        sum+=parseInt(bit)
-    }
-
-    return sumofDigits(sum);
+function findd(s,t){
+    let ssort = s.split('').sort();
+    return t.split('').sort.find((char, i) => char == ssort[i] )
 }
 
-console.log(sumofDigits(31))
+console.log("abcde","abcdef")
