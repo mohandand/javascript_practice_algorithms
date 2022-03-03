3b8dc862e7742151025ec6501978ae8032fa8474
mohandand <mohandand@gmail.com>
2022-03-02 16:51:04 -0800

    Maximum area

--- practise.js
+++ practise.js
@@ -1,11 +1,14 @@
-function vowelscount(str){
-    let count = [];
-    for(letter of str){
-        if(letter.match(/[aeiou]/g)){
-            count++;
-        }
+function maxArea(array){
+    let res = 0;
+    let left=array[0];
+    let right = array[array.length-1];
+    while(left<right){
+        let shortone = Math.min(array[left],array[right])
+        let area = shortone * (right-left)
+        if(area>res)  res=area;
+       (array[left]>array[right]) ? right-- : left++ 
     }
-    return count;
+  return res;
 }
- 
-console.log(vowelscount("aeioubbbb"))
+
+console.log(maxArea[1,8,6,2,5,4,8,3,7])
