32d344bbeecab0e8f90be5cde227f1abe015fbfb
mohandand <mohandand@gmail.com>
2022-03-05 13:12:24 -0800

    Product of array except itself code

--- ProdcutOfArrExcelptSelf.js
+++ ProdcutOfArrExcelptSelf.js
@@ -1,24 +1,15 @@
 var productExceptSelf = function(nums) {
-    let prefix = [];
-    let postfix=[nums.length];
-    let res=[];
-    let intial = 1;
-    let a =1
-    for(let i=0;i<nums.length;i++){
-        let mul = nums[i]*intial;
-        prefix.push(mul);
-        intial=mul
+    var output = [];
+    var leftMult = 1;
+    var rightMult = 1;
+    for (var i=nums.length - 1; i >= 0; i--) {
+        output[i] = rightMult;
+        rightMult *= nums[i];
     }
-    for(let i=nums.length-1;i>=0;i--){
-        let mul = nums[i]*a;
-        postfix[i]=mul;
-        a=mul
+    for (var j=0; j < nums.length; j++) {
+        output[j] *= leftMult;
+        leftMult *= nums[j];
     }
-    for(let i=1;i<nums.length-1;i++){
-       res[0]=postfix[1]
-       res[nums.length-1] =prefix[nums.length-2]
-       res[i] = prefix[i-1]*postfix[i+1]
-    }
-    return res;
+    return output;
 };
 console.log(productExceptSelf([0,0]))
