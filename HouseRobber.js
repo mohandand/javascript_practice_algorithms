a4ba6b5a0ac1c2ca9d627445aac7f38f09b2b03c
mohandand <mohandand@gmail.com>
2022-03-06 12:16:25 -0800

    House Robber

---
+++ HouseRobber.js
@@ -0,0 +1,14 @@
+function houseRob(nums){
+    if(!nums.length) return 0
+    if(nums.length==1) return nums[0];
+    if(nums.length==2) return Math.max(nums[0],nums[1]);
+    
+    var maxAtTwoBefore = nums[0];
+    var maxAtOneBefore = Math.max(nums[0],nums[1]);
+    for(let i=0;i<nums.length;i++){
+        maxAtCurrent = Math.max(maxAtTwoBefore+nums[i],maxAtOneBefore);
+        maxAtTwoBefore = maxAtOneBefore;
+        maxAtOneBefore = maxAtCurrent;
+    }
+    return maxAtOneBefore;
+}
