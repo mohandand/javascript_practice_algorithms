5bb3224cb41dc703d96ec98b699db5d6d5489dba
mohandand <mohandand@gmail.com>
2022-03-06 12:17:34 -0800

    House Robber

--- HouseRobber.js
+++ HouseRobber.js
@@ -5,7 +5,7 @@ function houseRob(nums){
     
     var maxAtTwoBefore = nums[0];
     var maxAtOneBefore = Math.max(nums[0],nums[1]);
-    for(let i=0;i<nums.length;i++){
+    for(let i=2;i<nums.length;i++){
         maxAtCurrent = Math.max(maxAtTwoBefore+nums[i],maxAtOneBefore);
         maxAtTwoBefore = maxAtOneBefore;
         maxAtOneBefore = maxAtCurrent;
