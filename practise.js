547dfceb9aee372db602f4e7de9e285495e6c425
mohandand <mohandand@gmail.com>
2022-03-17 11:30:12 -0700

    practise concepts

--- practise.js
+++ practise.js
@@ -1,5 +1,18 @@
-console.log(y())
-
-var y = function(){
-  return 0;
+function aplace(str){
+  let alpha = "abcdefghijklmnopqrstuvwxyz"
+  let alphaarr = alpha.split("");
+  let res = [];
+  let cstr = str.replace(/[^a-zA-Z]/g,'')
+  console.log(cstr)
+  for(let char of cstr){
+    
+     res.push((alphaarr.indexOf(char)) + 1);
+  } 
+ return res;
 }
+
+console.log(aplace("abcde#"))
+
+
+
+
