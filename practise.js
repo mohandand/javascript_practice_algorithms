7c1374333c2743af8ba625f70c7780a65f41e0b0
mohandand <mohandand@gmail.com>
2022-03-18 12:09:51 -0700

    practise programming

--- practise.js
+++ practise.js
@@ -1,6 +1,13 @@
-function rot(array){
-    return array.repeat(2).slice(1,-1).includes(array);
+function repat(a,b){
+    let num = Math.ceil(b.length/a.length);
+    if(a.repeat(num).includes(b)){
+        return num;
+    }
+    if(a.repeat(num+1).includes(b)){
+        return num+1;
+    }
+
+    return -1
 }
 
-console.log(rot("abcabc"))
-
+console.log(repat("a","aaa"))
