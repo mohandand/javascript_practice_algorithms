7d0d64573e7cee3dab7567693a886340e660c9c9
mohandand <mohandand@gmail.com>
2022-03-22 11:07:36 -0700

    practise programming

--- practise.js
+++ practise.js
@@ -1,6 +1,18 @@
-function rotatematrix(x){
-   let absres = Math.abs(x).toString().split("").reverse().join("")
-   return absres*Math.sign(x)
-  };
-   
-  console.log(rotatematrix(-123));
+function arrayself(array){
+   let lefta =[];
+   let leftpostion =1;
+   for(let i=0;i<array.length;i++){
+      lefta[i]=leftpostion;
+      leftpostion*=array[i]
+   }
+   let righa=[]
+   let rightpostion=1;
+   for(let i=0;i<array.lenght;i++){
+      righa[i] = rightpostion;
+      righa[i]*=rightpostion;
+      right[i]*=lefta[i]
+
+   }
+
+   return righa;
+}
