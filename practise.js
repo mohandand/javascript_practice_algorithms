dde86473c3ea1e4f71cd148eb38e398659d3011f
mohandand <mohandand@gmail.com>
2022-03-26 15:21:39 -0700

    practise programming

--- practise.js
+++ practise.js
@@ -1,2 +1,17 @@
-let auth = `Basic ${"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"}`
-console.log(auth)
+//Take input and output variable
+//Create array with all aplhabets.
+// remove other than alphabets form a string
+// loop through string and find it index and push it to res array.
+
+function postion(str){
+    let res = [];
+    let alpha = "abcdefghijklmenopqrstuvwxyz"
+    let alphabets = str.toLowerCase().replace(/[^a-zA-Z]/g,'');
+    let alphaarray = alpha.split("")
+    for(let char of alphabets){
+        res.push(alphaarray.indexOf(char) +1 )
+    }
+    return res;
+}
+
+console.log(postion("abc123"))
