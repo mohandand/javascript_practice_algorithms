f01c3aac2cb3d928d954073a7d90a47934a36a70
mohandand <mohandand@gmail.com>
2022-03-26 17:50:55 -0700

    practise programming

--- practise.js
+++ practise.js
@@ -1,2 +1,12 @@
-//loop through string and concatinate letters to new string
+// use reguler expression to check it includes any vowel
 
+function vowelcount(str){
+    let count=0;
+    for(let char of str){
+        if(char.match(/[aeiou]/g)){
+            count++;
+        }
+    }
+    return count;
+}
+console.log(vowelcount("aeiou"))
