08856250761759c57601153bdb88a1cfd891bbd3
mohandand <mohandand@gmail.com>
2022-03-27 11:29:44 -0700

    Coding Practice

--- practise.js
+++ practise.js
@@ -2,3 +2,12 @@
 
 // Then slice last values how many you need to put first and last part of array
 
+function rotate(arry,k){
+   let rota = k%arry.length
+   if(rota == 0) return arry
+   let res = arry.slice(0,arry.length-k);
+   arry.splice(0,arry.length-k)
+   return [...arry, ...res]
+}
+
+console.log(rotate([1,2,3,4,5] , 2))
