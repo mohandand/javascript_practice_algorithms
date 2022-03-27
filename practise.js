df994d967f6c0e0358af0d7964dca77d4fe157fc
mohandand <mohandand@gmail.com>
2022-03-27 10:39:06 -0700

    practise programming

--- practise.js
+++ practise.js
@@ -1,12 +1,18 @@
-// use reguler expression to check it includes any vowel
+// convert array to object key and value paris 
+// push unique elemnts into array and return length of array.
 
-function vowelcount(str){
-    let count=0;
-    for(let char of str){
-        if(char.match(/[aeiou]/g)){
-            count++;
-        }
+function runique(arr){
+  let obj={}
+  let res =[];
+  for(let i=0;i<arr.length;i++){
+    obj[arr[i]] ? obj[arr[i]]++ :obj[arr[i]]=1
+  }
+  for(item in obj){
+    if(obj[item] == 1){
+        res.push(item)
     }
-    return count;
+  }
+  return res;
 }
-console.log(vowelcount("aeiou"))
+
+console.log(runique([0,0,1,1,1,2,2,3,3,4]))
