b890211eb90c01fe344844ff7c509763e945a4a9
mohandand <mohandand@gmail.com>
2022-03-26 17:44:10 -0700

    practise programming

--- practise.js
+++ practise.js
@@ -1,13 +1,2 @@
-//loop through string and store it in object format
-//loop thorhgh object values and find for not equal to one
-function unique(str){
-    let obj ={};
-    for(let char of str){
-        obj[char] ? obj[char]++ :obj[char] = 1
-    }
-    let res = Object.values(obj).every((value) => value == 1 )
+//loop through string and concatinate letters to new string
 
-    return res;
-}
-
-console.log(unique("abcdefgaz"))
