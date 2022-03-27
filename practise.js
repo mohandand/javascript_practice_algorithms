712f6d0041a09b271a57c3714094d810718d8e63
mohandand <mohandand@gmail.com>
2022-03-27 10:51:00 -0700

    practise programming

--- practise.js
+++ practise.js
@@ -1,18 +1,9 @@
-// convert array to object key and value paris 
-// push unique elemnts into array and return length of array.
+// will buy on first day and profit =0
 
-function runique(arr){
-  let obj={}
-  let res =[];
-  for(let i=0;i<arr.length;i++){
-    obj[arr[i]] ? obj[arr[i]]++ :obj[arr[i]]=1
-  }
-  for(item in obj){
-    if(obj[item] == 1){
-        res.push(item)
-    }
-  }
-  return res;
+// on next day if buy is less than next day value then sell it other sie buy it and do the profit calculation
+
+function bss(arry){
+  let buy= arry[0];
+  let profit=0;
+  for(let i=0;i<arry.len)
 }
-
-console.log(runique([0,0,1,1,1,2,2,3,3,4]))
