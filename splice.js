ca899dad3e95484246053f661da63e2510dc6907
mohandand <mohandand@gmail.com>
2022-03-27 12:48:40 -0700

    Coding Practice

---
+++ splice.js
@@ -0,0 +1,9 @@
+let array = [1,2,3,4,5,6]
+
+// let res = array.splice(0,3) // first index is 
+
+// console.log(`removed elements ${res} and other elments on original array ${array}`);
+
+let res1 = array.splice(-1,2)
+
+console.log(`removed elements ${res1} and other elments on original array ${array}`); 
