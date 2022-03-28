a3cb312cb85ef2acc4fbfceec952cd4170a510b6
mohandand <mohandand@gmail.com>
2022-03-28 13:53:55 -0700

    Coding Practice

---
+++ practise.js
@@ -0,0 +1,17 @@
+//create objects for two arrays 
+
+//push values into array and decrease the count of number of keys in that object
+
+function ia(arr1,arr2){
+    let obj1 = {}
+    let obj2 = {}
+    let res = []
+    for(let i=0;i<arr1.length;i++){
+        obj1[arr1[i]]? obj1[arr1[i]]++ : obj1[arr1[i]]=1
+    }
+    for(let k of arr2){
+        if(obj1[k]>0){
+            res.push(item)
+        }
+    }
+}
---
+++ slice.js
@@ -0,0 +1,5 @@
+let array = [1,2,3,4,5,6]
+
+let res = array.splice(1,-2) // first index is and untill
+
+ console.log(`removed elements ${res} and other elments on original array ${array}`);
