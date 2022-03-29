3d11ad91de10930f4e8bcc95a8473955a14f6cf3
mohandand <mohandand@gmail.com>
2022-03-29 16:15:27 -0700

    Coding Practice

--- practise.js
+++ practise.js
@@ -1,11 +1,27 @@
-function anagram(array){
-    let sortedarray = [];
-    let sortedObject = {}
-    for(let i=0;i<array.length;i++){
-        sortedarray.push(array[i].sort())
+function anagram(strs){
+    var groupAnagrams = function(strs) {
+        let sortedarray = [];
+        let sortedObject = {}
+        let res =[];
+        let temp =[];
+    for(let i=0;i<strs.length;i++){
+        let sortedword = strs[i].split('').sort().join("")
+        sortedarray.push(sortedword)
     }
-    for(let i=0;i<sortedarray;i++){
+    for(let i=0;i<sortedarray.length;i++){
         sortedObject[sortedarray[i]] ? sortedObject[sortedarray[i]]++ : sortedObject[sortedarray[i]] =1
     }
-    console.log(anagram(["cat" , "act","bat","tab","atb"]))
-}
+   for(let item in sortedObject){
+       for(var i=0 ;i<sortedarray.length;i++){
+           if(item == sortedarray[i]){
+               temp.push(strs[i])
+           }
+       }
+          res.push([...temp])
+           temp.length=0;
+   }
+   return res
+};
+}  
+
+console.log(anagram(["cat","act","bat","tab","atb","tac"]))
