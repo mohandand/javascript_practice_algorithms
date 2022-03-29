b1198b80c7a399807cf0cb8cd3a57fcc8423f256
mohandand <mohandand@gmail.com>
2022-03-29 14:35:47 -0700

    Coding Practice

--- practise.js
+++ practise.js
@@ -1,19 +1,11 @@
-//create objects for two arrays 
-
-//push values into array and decrease the count of number of keys in that object
-
-function ia(arr1,arr2){
-    let obj1 = {}
-    let obj2 = {}
-    let res = []
-    for(let i=0;i<arr1.length;i++){
-        obj1[arr1[i]]? obj1[arr1[i]]++ : obj1[arr1[i]]=1
+function anagram(array){
+    let sortedarray = [];
+    let sortedObject = {}
+    for(let i=0;i<array.length;i++){
+        sortedarray.push(array[i].sort())
     }
-    for(let k of arr2){
-        if(obj1[k]>0){
-            res.push(k)
-        }
+    for(let i=0;i<sortedarray;i++){
+        sortedObject[sortedarray[i]] ? sortedObject[sortedarray[i]]++ : sortedObject[sortedarray[i]] =1
     }
-    return res
+    console.log(anagram(["cat" , "act","bat","tab","atb"]))
 }
-console.log(ia([1,2,3,4,3],[1,2,3,3]))
