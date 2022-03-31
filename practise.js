f599ae753dbe73d9faf1b4411edf9b876c557dcf
mohandand <mohandand@gmail.com>
2022-03-31 13:14:30 -0700

    Coding Practice

--- practise.js
+++ practise.js
@@ -1,12 +1,17 @@
 function findEvenIndex(arr)
 {
   let stsum = []
+  let revsum =[]
   let count = 0;
   for(let i=0;i<arr.length;i++){
     count = count + arr[i]
     stsum[i] = count;
   }
-  return stsum;
+  for(let i=arr.length-1;i>=0;i--){
+    count = count + arr[i]
+    revsum[i] = count;
+  }
+  return revsum
 }
 
 console.log(findEvenIndex([1,2,3,-4]))
