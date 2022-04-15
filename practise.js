13164c7508d05447d4a7b149361764449b6e82cc
mohandand <mohandand@gmail.com>
2022-04-15 10:24:47 -0700

    Coding Practise

--- practise.js
+++ practise.js
@@ -1,12 +1,13 @@
-var twoSum = function(nums, target) {
-  let map= {}
-  for(let i=0;i<nums.length;i++){
-      let diff = target - nums[i]
-      console.log(diff)
-      if(map[diff] !== undefined){
-          return [map[diff] , i]
+var maxProfit = function(prices) {
+  let buy = prices[0];
+  let profit =0;
+      for(let i=1;i<prices.length;i++){
+          if(buy>prices[i]){
+              buy = prices[i]
+          }else{
+              profit = Math.max(profit,prices[i]-buy)
+          }
       }
-      map[nums[i]] = i
-    }
-  }
-  console.log(twoSum([2,7,11,15],9));
+  return profit
+  };
+  console.log(maxProfit([7,1,5,3,6,4]));
