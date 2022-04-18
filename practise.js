8c72a9b70e4dec043bd8964946eaed286ad1b8e9
mohandand <mohandand@gmail.com>
2022-04-18 15:03:49 -0700

    Coding Practise

--- practise.js
+++ practise.js
@@ -2,16 +2,13 @@ var maxProduct = function(nums) {
   let maxsoFar=nums[0]
   let minsoFar=nums[0]
   let res = nums[0]
+  let tempmaxsoFar;
   for(let i=1;i<nums.length;i++){
-      maxsoFar = Math.max(nums[i],maxsoFar*nums[i])
-      minsoFar = Math.min(nums[i], minsoFar*nums[i])
-      if(nums[i]<0){
-          let temp = maxsoFar;
-          maxsoFar = minsoFar
-          minsoFar = temp
-      }
+      tempmaxsoFar = Math.max(nums[i],maxsoFar * nums[i],minsoFar * nums[i])
+      minsoFar = Math.min(nums[i],maxsoFar * nums[i],minsoFar * nums[i])
+      maxsoFar = tempmaxsoFar
       res = Math.max(res, maxsoFar)
   }
   return res
 };
-console.log(maxsubarray([-2,1,-3,4,-1,2,1,-5,4]))
+console.log(maxProduct([2,3,-2,4,-3]))
