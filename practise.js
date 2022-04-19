af18a135d27021d9892ef61aa9ecfdf3da62aa2e
mohandand <mohandand@gmail.com>
2022-04-19 12:36:12 -0700

    Coding Practise

--- practise.js
+++ practise.js
@@ -1,11 +1,13 @@
-function alpha(str){
-  let alpha = "abcdefghijklmnopqrstuvwxyz"
-  let alphaarra = alpha.split('');
-  let inputstr = str.toLowerCase().replace(/[^a-zA-Z]/g, '');
-  let res =[];
-   for(let i of str){
-     res.push(inputstr.indexOf(i) + 1)
-   }
-   return res;
+function uni(str){
+  let obj ={};
+  for(let char of str){
+    obj[char]?obj[char]++:obj[char]=1
+  }
+  for(let item in obj){
+    if(obj[item]>1){
+      return false
+    }
+  }
+  return true
 }
-console.log(alpha("abcdeg"))
+console.log(uni("123abcc"))
