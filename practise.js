a1957f282ddf14f952261fece671e47a5d038a89
mohandand <mohandand@gmail.com>
2022-04-19 13:35:19 -0700

    Coding Practise

--- practise.js
+++
@@ -1,7 +0,0 @@
-function lcp(strarr){
- for(let i =0 ;i<strarr[0].length;i++){
-   for(let j=0;j<strarr.length;j++){
-        if(strarr[0][i] !== strarr[j][i]) return strarr[0].slice(0,i)
-   }
- }
-}
