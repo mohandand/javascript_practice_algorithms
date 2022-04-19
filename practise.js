dc7296e699695db65ec3ea8973bc512f7a6688dd
mohandand <mohandand@gmail.com>
2022-04-19 12:37:19 -0700

    Coding Practise

--- practise.js
+++ practise.js
@@ -4,10 +4,10 @@ function uni(str){
     obj[char]?obj[char]++:obj[char]=1
   }
   for(let item in obj){
-    if(obj[item]>1){
-      return false
+    if(obj[item]==1){
+      return item
     }
   }
   return true
 }
-console.log(uni("123ab"))
+console.log(uni("11223aab"))
