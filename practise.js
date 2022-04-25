110241497a4a022a84d40235ab7ca2ed7252a51a
mohandand <mohandand@gmail.com>
2022-04-25 12:34:12 -0700

    Coding Practise

--- practise.js
+++ practise.js
@@ -1,31 +1,19 @@
-var minWindow = function (s, t) {
-    let output = "";
-    let [left, right] = [0, -1];
-    let map = {};
-  
-    for (let i = 0; i < t.length; i++) map[t[i]] = map[t[i]] + 1 || 1;
-    let uniqueChars = Object.keys(map).length;
-  
-    while (right <= s.length) {
-      // if we found a valid substring
-      if (uniqueChars == 0) {
-        let current = s[left];
-        if (map[current] != null) map[current]++;
-        if (map[current] > 0) uniqueChars++;
-  
-        let temp = s.substring(left, right + 1);
-        if (output == "") output = temp;
-        else output = output.length < temp.length ? output : temp;
-        left++;
-      } else {
-        right++;
-        let current = s[right];
-        if (map[current]!= null) map[current]--;
-        if (map[current] == 0) uniqueChars--;
+//Valid Paranthesis
+var isValid = function(s){
+  let bracket = {
+      '(':')',
+      '{':'}',
+      '[':']'
+  }
+  let heap= [];
+  for(char of s){
+      if(bracket[char]){
+          heap.push(bracket[char]);
+      }else{
+          if(!heap.pop()==char) return false
       }
-    }
-    return output;
-  };
+  }
+  return (!heap.length)
+}
 
-  console.log(minWindow("ADOBECODEBANC","ABC"))
-  
+console.log(isValid('(]'));
