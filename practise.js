3ef6ac4ce51dbc133b83af0555e5f008c7c2f0ab
mohandand <mohandand@gmail.com>
2022-04-25 13:46:41 -0700

    Coding Practise

--- practise.js
+++ practise.js
@@ -1,19 +1,16 @@
-//Valid Paranthesis
-var isValid = function(s){
-  let bracket = {
+var isValid = function(s) {
+  let stack = [];
+  let par = {
       '(':')',
-      '{':'}',
-      '[':']'
+      '{' : '}',
+      '[' : ']'
   }
-  let heap= [];
-  for(char of s){
-      if(bracket[char]){
-          heap.push(bracket[char]);
+  for(let char of s){
+      if(par[char]){
+          stack.push(par[char])
       }else{
-          if(!heap.pop()==char) return false
+          if(stack.pop() !== char) return false
       }
   }
-  return (!heap.length)
-}
-
-console.log(isValid('(]'));
+  return (!stack.length)
+};
