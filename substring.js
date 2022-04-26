7be7704310f76a834f62768b0f19d5bcdffb3a80
mohandand <mohandand@gmail.com>
2022-04-26 12:15:09 -0700

    Coding Practise

---
+++ substring.js
@@ -0,0 +1,9 @@
+function substring(s){
+    let res =[];
+    for(let i = 0;i<s.length;i++){
+        for(let j= i+1;j<s.length;j++){
+             res.push(s.substring(i,j))
+        }
+    }
+    return 
+}
