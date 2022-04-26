b5d18d8f779c50a90979bdc5623a1ec19384af24
mohandand <mohandand@gmail.com>
2022-04-26 16:05:19 -0700

    Coding Practise

--- practise.js
+++ practise.js
@@ -1,17 +1,24 @@
-var countSubstrings = function(s) {
-    let count=0;
+var longestPalindromeSubseq = function(s) {
+     let max=0;
     const arr=[];
     for(let i=0;i<s.length;i++){
         countPalindrome(s,i,i); // odd length
         countPalindrome(s,i,i+1); // even length
+        
     }
-    return count;
+    return arr[0];
     function countPalindrome(str,left,right){
         while(left>=0 && right<str.length && str[left]===str[right]){
-			arr.push(str.substring(left,right+1)); // if you want those substrings as well
-            count++;
+            let temp =str.substring(left,right+1);
+            if(max > temp.length){
+                max  = max
+            }else{
+               max = temp.length
+               arr[0] =temp
+            }
             left--;
             right++;
         }
     }
 };
+console.log(longestPalindromeSubseq("bbbab"))
