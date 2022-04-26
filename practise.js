f458db95d95db94e057354df0474f2a63b137875
mohandand <mohandand@gmail.com>
2022-04-26 16:06:29 -0700

    Coding Practise

--- practise.js
+++ practise.js
@@ -1,24 +1,27 @@
-var longestPalindromeSubseq = function(s) {
-     let max=0;
-    const arr=[];
-    for(let i=0;i<s.length;i++){
-        countPalindrome(s,i,i); // odd length
-        countPalindrome(s,i,i+1); // even length
-        
-    }
-    return arr[0];
-    function countPalindrome(str,left,right){
-        while(left>=0 && right<str.length && str[left]===str[right]){
-            let temp =str.substring(left,right+1);
-            if(max > temp.length){
-                max  = max
-            }else{
-               max = temp.length
-               arr[0] =temp
-            }
-            left--;
-            right++;
-        }
-    }
+/**
+ * @param {string} s
+ * @return {string}
+ */
+ var longestPalindrome = function(s) {
+    let max=0;
+   const arr=[];
+   for(let i=0;i<s.length;i++){
+       countPalindrome(s,i,i); // odd length
+       countPalindrome(s,i,i+1); // even length
+       
+   }
+   return arr[0];
+   function countPalindrome(str,left,right){
+       while(left>=0 && right<str.length && str[left]===str[right]){
+           let temp =str.substring(left,right+1);
+           if(max > temp.length){
+               max  = max
+           }else{
+              max = temp.length
+              arr[0] =temp
+           }
+           left--;
+           right++;
+       }
+   }
 };
-console.log(longestPalindromeSubseq("bbbab"))
