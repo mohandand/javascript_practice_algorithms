01f2ab78f4d7fa84ca4f4f441c7a0c0532772bd4
mohandand <mohandand@gmail.com>
2022-04-26 12:52:46 -0700

    Coding Practise

--- practise.js
+++ practise.js
@@ -1,11 +1,23 @@
-function substring(s){
-  let res =[];
-  for(let i = 0;i<s.length;i++){
+var longestPalindromeSubseq = function(s) {
+  let maxlength = 0;
+  for(let i=0;i<=s.length;i++){
       for(let j= i+1;j<=s.length;j++){
-           res.push(s.substring(i,j))
+          let temp = s.substring(i,j)
+          if(isPalindrome(temp)){
+              maxlength = Math.max(maxlength, temp.length)
+          }
       }
   }
-  return res;
-}
+  return maxlength
+  function isPalindrome(str){
+      let l=0;
+      let r=str.length-1;
+      while(l<=r && str.charAt(l)===str.charAt(r)){
+          l++;
+          r--;
+      }
+      return l>=r;
+  }
+};
 
-console.log(substring("abc"))
+console.log(longestPalindromeSubseq("abc"))
