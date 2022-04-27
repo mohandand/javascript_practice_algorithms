ad1888f3f5cbc47dc5277a75906045a8128c4991
mohandand <mohandand@gmail.com>
2022-04-27 12:16:56 -0700

    Coding Practise

--- practise.js
+++ practise.js
@@ -1,13 +1,13 @@
 function findLargestDiff(array){
-    let newarray = [...array];
-    newarray.sort((a,b) => {return a-b})
-    for(let j=array.length-1 ;j>=0;j--){
-        for(let i=0;i<array.length;i++){
-            let temp = newarray[j]-newarray[i]
-            if(array.indexOf(newarray[i])<array.indexOf(newarray[j])){
-                return temp
-            }
+    let currenmin = array[0];
+    let currmaxdiff = 0;
+    for(let i=1;i<array.length;i++){
+        if(array[i]>currenmin && (array[i]-currenmin>currmaxdiff)){
+            currmaxdiff = array[i]-currenmin
+        }else if(array[i]<currenmin){
+            currenmin =array[i]
         }
     }
+    return currmaxdiff
 }
-console.log(findLargestDiff([7, 8, 8, 9, 9, 1, 3, 1, 10]))
+console.log(findLargestDiff([7, 8, 4, 9, 9, 15, 3, 1, 10]))
