aaad6f68d280a18237b4da724fef095f05b335d3
mohandand <mohandand@gmail.com>
2022-04-27 11:12:24 -0700

    Coding Practise

--- practise.js
+++ practise.js
@@ -1,40 +1,12 @@
-// function minarray(array,target){
-//     array.sort((a,b) => {return b-a})
-//     let sum =0;
-//     let count =0;
-//   for(let i=0;i<array.length;i++){
-//       sum+=array[i]
-//       count++
-//       if(sum>=target){
-//           return count;
-//       }
-//   }
-// }
-// console.log(minarray([1,4,45,6,0,19],51))
-
-// function minarray(array,target){
-//     let count =0;
-//     let sum =0;
-//     let end ;
-//     for(let i =0;i<array.length;i++){
-//         sum = sum+array[i]
-//         count++
-//         if(sum>target){
-//             end =i;
-//             break;
-//         }
-//     }
-//     for(let i=0;i<array.length;i++){
-//         sum = sum - array[i]
-//         count--;
-//         if(sum>target){
-//             continue
-//         }else{
-//             console.log(array.slice(i,end+1))
-//             return count+1
-//         }
-//     }  
-// }
-console.log(minarray([1,4,45,6,0,19],51))
-console.log(minarray([1,10,5,2,7],9))
-console.log(minarray([9,4,1,23,3,26],25))
+function decimalToBinary(num){
+    if(num>=1){
+       if(num%2){
+           return decimalToBinary((num-1)/2) + 1
+       }else{
+           return decimalToBinary(num/2) + 0
+       }
+    }else{
+        return ''
+    }
+}
+console.log(decimalToBinary(5))
--- smallestSubArraySum.js
+++ smallestSubArraySum.js
@@ -25,3 +25,51 @@ function smallestSubWithSum(arr, n, x)
 }
 
 console.log(smallestSubWithSum([9,4,1,23,3,26],6,25))
+
+
+
+
+
+
+
+
+
+// function minarray(array,target){
+//     array.sort((a,b) => {return b-a})
+//     let sum =0;
+//     let count =0;
+//   for(let i=0;i<array.length;i++){
+//       sum+=array[i]
+//       count++
+//       if(sum>=target){
+//           return count;
+//       }
+//   }
+// }
+// console.log(minarray([1,4,45,6,0,19],51))
+
+// function minarray(array,target){
+//     let count =0;
+//     let sum =0;
+//     let end ;
+//     for(let i =0;i<array.length;i++){
+//         sum = sum+array[i]
+//         count++
+//         if(sum>target){
+//             end =i;
+//             break;
+//         }
+//     }
+//     for(let i=0;i<array.length;i++){
+//         sum = sum - array[i]
+//         count--;
+//         if(sum>target){
+//             continue
+//         }else{
+//             console.log(array.slice(i,end+1))
+//             return count+1
+//         }
+//     }  
+// console.log(minarray([9,4,1,23,3,26],25))}
+// console.log(minarray([1,4,45,6,0,19],51))
+// console.log(minarray([1,10,5,2,7],9))
